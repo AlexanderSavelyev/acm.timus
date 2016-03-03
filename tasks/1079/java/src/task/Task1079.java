8f0ff379ed1fac374989fdd72f6ed93386e54fe3
alexander savelev <aleksandr_savelev@epam.com>
2016-03-04 01:44:14 +0300

    fix task 1079

--- tasks/1079/java/src/task/Task1079.java
+++ tasks/1079/java/src/task/Task1079.java
@@ -16,7 +16,7 @@ public class Task1079 {
     * @param args the command line arguments
     */
    public static void main(String[] args) throws IOException {
-      new Task1079().solve();
+      new Task1079().run();
    }
    StreamTokenizer in;
    PrintWriter out;
@@ -28,22 +28,52 @@ public class Task1079 {
 
    void run() throws IOException {
       boolean oj = System.getProperty("ONLINE_JUDGE") != null;
-      //Reader reader = oj ? new InputStreamReader(System.in) : new FileReader("../input.txt");
-      Reader reader = new FileReader("../input.txt");
+      Reader reader = oj ? new InputStreamReader(System.in) : new FileReader("../input.txt");
+      //Reader reader = new FileReader("../input.txt");
       //Writer writer = oj ? new OutputStreamWriter(System.out) : new FileWriter("output.txt");\
-      Writer writer =  new OutputStreamWriter(System.out);
+      Writer writer = new OutputStreamWriter(System.out);
       in = new StreamTokenizer(new BufferedReader(reader));
       out = new PrintWriter(writer);
 
       solve();
       out.flush();
    }
-
+   int[] a;
    void solve() throws IOException {
-      int a = nextInt();
-      int b = nextInt();
-      out.print(a + b);
-      out.print(" ");
-      out.println(a - b);
+      a = new int[100000];
+      a[1]=1;
+      int n = 0;
+      while ((n = nextInt()) > 0) {
+         int start = n >> 1;
+         if(start % 2 == 0) {
+            start -= 1;
+         }
+         if (start <=0) {
+            start = 1;
+         }
+         int max = 0;
+         int next = 0;
+         for (int j = start; j <=n; j+=2) {
+            next = calcElement(j);
+            if (next > max) {
+               max = next;
+            }
+         }
+         out.println(max);
+      }
    }
+   
+   int calcElement(int idx) {
+      if (a[idx] > 0 || idx == 0) {
+         return a[idx];
+      }
+      int j = idx >> 1;
+      if(j<<1 == idx) {
+         a[idx] = calcElement(j);
+      } else {
+         a[idx] = calcElement(j) + calcElement(j+1);
+      }
+      return a[idx];
+   }
+   
 }
