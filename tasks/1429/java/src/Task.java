6596bc7fab2178350cdd00313ea19edf00670b66
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-03-07 22:12:57 +0300

    add link

--- tasks/1429/java/src/Task.java
+++ tasks/1429/java/src/Task.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Aleksandr_Savelev
+ * http://acm.timus.ru/problem.aspx?space=1&num=1429
  */
 public class Task {
 
