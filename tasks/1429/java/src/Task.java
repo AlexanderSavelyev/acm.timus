86cf9d87db7c7cca784c1e4d77ed34601910e15e
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-03-10 00:50:41 +0300

    prepare template test

--- tasks/1429/java/src/Task.java
+++ tasks/1429/java/src/Task.java
@@ -1,14 +1,29 @@
-/**
- *
- * http://acm.timus.ru/problem.aspx?space=1&num=1429
- */
+import java.io.*;
+
 public class Task {
 
-   /**
-    * @param args the command line arguments
-    */
-   public static void main(String[] args) {
-      // TODO code application logic here
+   public static void main(String[] args) throws IOException {
+      Reader reader = new InputStreamReader(System.in);
+      Writer writer = new OutputStreamWriter(System.out);
+      new Task().run(reader, writer);
    }
+   StreamTokenizer in;
+   PrintWriter out;
+
+   int nextInt() throws IOException {
+      in.nextToken();
+      return (int) in.nval;
+   }
+
+   void run(Reader reader, Writer writer) throws IOException {
+      in = new StreamTokenizer(new BufferedReader(reader));
+      out = new PrintWriter(writer);
+      solve();
+      out.flush();
+   }
+   void solve() throws IOException {
+         out.println(1);
+   }
+   
    
 }
