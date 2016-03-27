923e1e0aa80533324f36ddc0ce96c55a9831823d
Alexander Savelyev <aleksandr_savelev@epam.com>
2016-03-28 02:15:46 +0300

    add cool test

--- tasks/1000/cpp/test.cpp
+++ tasks/1000/cpp/test.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <assert.h>
+#include <cstring>
 #include "main.cpp"
+
+bool testAssert(const char* exp, stringstream& res) {
+   return res.str().compare(exp) == 0;
+}
+
+string toString(stringstream& res) {
+   return res.str();
+}
+#define assertEq(expr1, expr2) \
+    do { \
+        if (false == testAssert(expr1, expr2)) { \
+            std::cerr <<"ASSERTION FAILED: " \
+                      << ::basename(__FILE__) << ":" << __LINE__ \
+                      << ", expected '" << expr1 << "' but was '" << toString(expr2)<< "'" << std::endl; \
+        } \
+    } while (0)
 
 using namespace std;
 
 int main() {
    Task task;
-   task.run(cin, cout);
+   stringstream s_in;
+   s_in<<1<<"\n"<<2;
+   
+   stringstream s_out;
+   task.run(s_in, s_out);
+   
+   assertEq("4", s_out);
    
    return 0;
 }
