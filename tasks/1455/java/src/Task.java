63dd02c9512e5edc64f4fa912f9cdf75fe11a517
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-04-24 00:57:59 -0400

    pass 8

--- tasks/1455/java/src/Task.java
+++ tasks/1455/java/src/Task.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 
 
@@ -10,7 +9,54 @@ import java.util.LinkedList;
 public class Task {
    
 
-
+   public class Pair<A, B> {
+      public A first;
+      public B second;
+      public Pair(A first, B second) {
+         super();
+         this.first = first;
+         this.second = second;
+      }
+      @Override
+      public int hashCode() {
+         int hashFirst = first != null ? first.hashCode() : 0;
+         int hashSecond = second != null ? second.hashCode() : 0;
+         return (hashFirst + hashSecond) * hashSecond + hashFirst;
+      }
+      @Override
+      public boolean equals(Object other) {
+         Pair otherPair = (Pair) other;
+         return ((this.first == otherPair.first
+                 || (this.first != null && otherPair.first != null
+                 && this.first.equals(otherPair.first)))
+                 && (this.second == otherPair.second
+                 || (this.second != null && otherPair.second != null
+                 && this.second.equals(otherPair.second))));
+      }
+      @Override
+      public String toString() {
+         return "(" + first + ", " + second + ")";
+      }
+   }
+   
+   class UsageTree {
+      public HashMap<Pair<Integer, Integer>, HashSet<Pair<Integer, Integer>>> adj = new HashMap<>();
+      public boolean addEdge(Pair<Integer, Integer> from, Pair<Integer, Integer> to) {
+         if(!adj.containsKey(from)) {
+            adj.put(from, new HashSet<>());
+         }
+         if(adj.get(from).contains(to)) {
+            return false;
+         }
+         adj.get(from).add(to);
+         return true;
+      }
+      public void removeEdge(Pair<Integer, Integer> from, Pair<Integer, Integer> to) {
+         adj.get(from).remove(to);
+      }
+   }
+   
+   
    public static void main(String[] args) throws IOException {
       Reader reader = new InputStreamReader(System.in);
       Writer writer = new OutputStreamWriter(System.out);
@@ -40,18 +86,22 @@ public class Task {
    }
    
    ArrayList<String> words = new ArrayList<>();
-   HashSet<String> wordSet = new HashSet<>();
+   HashMap<String, Integer> wordMap = new HashMap<>();
    HashSet<Integer> wordSizes = new HashSet<>();
    HashMap<String, WordList> prefixes = new HashMap<>();
    HashSet<Integer> calculatedPrefixes = new HashSet<>();
-   
+   UsageTree usageTree = new UsageTree();
 
-   private boolean buildExperssion(StringBuilder t, int curPos) {
+   private boolean buildExperssion(StringBuilder t, int curPos, Pair<Integer, Integer> curUsage) {
       if(t.length() > 20000) {
+//         throw new RuntimeException();
          return false;
       }
+      int curLength = t.length();
+      int curUsageIdx = (curLength - curPos);
+      
       String curWord = t.substring(curPos);
-      if(wordsContains(curWord)) {
+      if(wordsContain(curWord) != null) {
          return true;
       }
       
@@ -60,23 +110,33 @@ public class Task {
             continue;
          }
          String curSubWord = curWord.substring(0, subWordSize);
-         if(wordsContains(curSubWord)) {
-            if(buildExperssion(t, curPos + curSubWord.length())) {
+         Integer exWord = wordsContain(curSubWord);
+         if (exWord != null) {
+            Pair<Integer, Integer> to = new Pair(curUsageIdx, exWord);
+            if (!usageTree.addEdge(curUsage, to)) {
+               return false;
+            }
+            if (buildExperssion(t, curPos + curSubWord.length(), to)) {
                return true;
             }
+            usageTree.removeEdge(curUsage, to);
          }
       }
       
       WordList w1 = getAllWordsContainedPrefix(curWord);
-      for (Iterator<Integer> iterator = w1.list.descendingIterator(); iterator.hasNext();) {
-         Integer curBigIdx = iterator.next();
-//      for (Integer curBigIdx : w1.list.descendingIterator()) {
+      for (Integer curBigIdx : w1.list) {
          String curBigWord = words.get(curBigIdx).substring(curWord.length());
-         int curLength = t.length();
+         
+//         System.out.println("len = " + curUsageIdx + " w = " + curBigIdx);
+         Pair<Integer, Integer> to = new Pair(curUsageIdx, curBigIdx);
+         if (!usageTree.addEdge(curUsage, to)) {
+            return false;
+         }
          t.append(curBigWord);
-         if(buildExperssion(t, curLength)) {
+         if(buildExperssion(t, curLength, to)) {
             return true;
          }
+         usageTree.removeEdge(curUsage, to);
          t.setLength(curLength);
       }
       
@@ -87,8 +147,8 @@ public class Task {
       return getAllWordsContainedPrefix(words.get(w));
    }
    
-   private boolean wordsContains(String tStr) {
-      return wordSet.contains(tStr);
+   private Integer wordsContain(String tStr) {
+      return wordMap.get(tStr);
    }
 
    private WordList getAllWordsContainedPrefix(String prefix) {
@@ -126,11 +186,11 @@ public class Task {
       
       for (int i = 0; i < N; i++) {
          String w = nextString();
+         wordMap.put(w, words.size());
          words.add(w);
-         wordSet.add(w);
          wordSizes.add(w.length());
       }
-      if(words.size() != wordSet.size()) {
+      if(words.size() != wordMap.size()) {
          throw new RuntimeException();
       }
       
@@ -140,7 +200,7 @@ public class Task {
          for (Integer curBigWord : w1.list) {
             t.append(words.get(curBigWord));
             int curPos = words.get(curWord).length();
-            if(buildExperssion(t, curPos)) {
+            if(buildExperssion(t, curPos, new Pair(0, curWord))) {
                out.println("YES");
                out.println(t.toString());
                return;
