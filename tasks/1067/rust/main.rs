a7b61a3b8b9382c4bc2844dc729830030a0274d3
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-05-27 01:13:11 +0300

    start 1067

---
+++ tasks/1067/rust/main.rs
@@ -0,0 +1,81 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+use std::collections::BTreeMap;
+
+struct Node {
+    pub nodes: BTreeMap<String, Option<Node>>
+}
+
+impl Node {
+    fn new()->Node {
+        Node {
+            nodes: BTreeMap::new()
+        }
+    }
+}
+
+fn solve(input: &mut Read, output: &mut Write) {
+    let mut reader = BufReader::new(input);
+    let mut input = String::new();
+
+    reader.read_line(&mut input).unwrap();
+    let n: i32 = input.trim().parse().unwrap();
+
+    let mut root = Node::new();
+
+    for _ in 1..n {
+        let mut currentNode:Option<&Node> = Some(&root);
+        let mut parent: Option<&Node> = None;
+        input.clear();
+        reader.read_line(&mut input).unwrap();
+
+        let mut parentName:Option<String> = None;
+
+        for name in input.trim().split("\\") {
+            println!("{}", name);
+
+            if currentNode.is_none() && parent.is_some() {
+                parent.unwrap().nodes.insert(parentName.unwrap(), Some(Node::new()));
+            }
+
+        }
+
+        // let a_str = s.next().unwrap();
+        // let a: i32 = a_str.trim().parse().unwrap();
+
+        // let b_str = s.next().unwrap();
+        // let b: i32 = b_str.trim().parse().unwrap();
+
+        // println!("{} {}", a,b);
+    }
+
+    println!("{}", n);
+
+
+}
+
+fn main() {
+    solve(&mut io::stdin(), &mut io::stdout());
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use solve;
+
+    #[test]
+    fn basic_test() {
+        let mut f = File::open("../input.txt").expect("correct test");
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut f, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+        //assert_eq!(res,
+//                   "2297.0716
+//936297014.1164
+//0.0000
+//37.7757
+//");
+    }
+}
