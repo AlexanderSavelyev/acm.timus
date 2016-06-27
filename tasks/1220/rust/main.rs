26a655699138d01b13e5e7bebd58510262bb61d9
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-06-28 01:39:25 +0300

    add working solution

--- tasks/1220/rust/main.rs
+++ tasks/1220/rust/main.rs
@@ -1,18 +1,63 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 
-use std::collections::BTreeMap;
-
 fn solve(input: &mut Read, output: &mut Write) {
     let mut reader = BufReader::new(input);
     let mut input = String::new();
 
+    let stack_num = 1000;
+
+
     reader.read_line(&mut input).unwrap();
     let n: i32 = input.trim().parse().unwrap();
-    for i in 1..n {
+
+    let mut st_values: Vec<u32> = Vec::new();
+    let mut st_nodes: Vec<usize> = Vec::new();
+
+    let mut last_idx: Vec<usize> = Vec::with_capacity(stack_num);
+
+    for _ in 0..stack_num {
+        last_idx.push(0);
+    }
+
+    for _ in 0..n {
         input.clear();
         reader.read_line(&mut input).unwrap();
-        println!("{:?}", input);
+        // println!("{:?}", input);
+        let mut v_in = input.trim().split(' ');
+        let op = v_in.next().unwrap();
+
+        if op.starts_with("PU") {
+            let st_id: u32 = v_in.next().unwrap().parse().unwrap();
+            let st_val: u32 = v_in.next().unwrap().parse().unwrap();
+
+            let st_idx = st_id as usize - 1;
+
+            //println!("{:?}, {}", st_id, st_val);
+
+            st_values.push(st_val);
+            let cur_idx = st_values.len();
+
+
+            let cur_node = last_idx[st_idx];
+            st_nodes.push(cur_node);
+
+            last_idx[st_idx] = cur_idx;
+
+        } else {
+            let st_id: u32 = v_in.next().unwrap().parse().unwrap();
+            let st_idx = st_id as usize - 1;
+
+            let cur_idx = last_idx[st_idx];
+
+
+            let st_val = st_values[cur_idx - 1];
+            //println!("{:?}", st_val);
+            writeln!(output, "{}", st_val).expect("correct output");
+
+            last_idx[st_idx] = st_nodes[cur_idx - 1];
+        }
+
     }
     
 
@@ -36,6 +81,6 @@ mod tests {
         solve(&mut f, &mut buf);
 
         let res = String::from_utf8(buf).expect("valid string");
-        //assert_eq!(res, "GAMES\n DRIVERS\nHOME\nWIN\n SOFT\nWINNT\n DRIVERS\n SYSTEM32\n  CERTSRV\n   CERTCO~1\n    X86\n  CONFIG\n");
+        assert_eq!(res, "400\n200\n300\n");
     }
 }
