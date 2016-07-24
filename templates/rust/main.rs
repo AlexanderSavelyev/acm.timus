0e676693687bb461bfbec32990ea18a3cf03773a
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2016-07-24 01:20:58 -0400

    update rust template

--- templates/rust/main.rs
+++ templates/rust/main.rs
@@ -9,21 +9,22 @@ fn solve(input: &mut Read, output: &mut Write) {
     reader.read_line(&mut input).unwrap();
     let n: i32 = input.trim().parse().unwrap();
 
-    for _ in 1..n {
+    for _ in 0..n {
         input.clear();
         reader.read_line(&mut input).unwrap();
-        let mut s = input.trim().split(' ');
+        println!("{:?}", input);
+        // let mut s = input.trim().split(' ');
 
-        let a_str = s.next().unwrap();
-        let a: i32 = a_str.trim().parse().unwrap();
+        // let a_str = s.next().unwrap();
+        // let a: i32 = a_str.trim().parse().unwrap();
 
-        let b_str = s.next().unwrap();
-        let b: i32 = b_str.trim().parse().unwrap();
+        // let b_str = s.next().unwrap();
+        // let b: i32 = b_str.trim().parse().unwrap();
 
-        println!("{} {}", a,b);
+        // println!("{} {}", a,b);
     }
 
-    println!("{}", n);
+    // println!("{}", n);
 
 
 }
@@ -39,9 +40,17 @@ mod tests {
 
     #[test]
     fn basic_test() {
-        let mut f = File::open("../input.txt").expect("correct test");
+        let test = String::from("5
+              ab
+              acb
+              bc
+              abac
+              babbc");
+        //let mut f = File::open("../input.txt").expect("correct test");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
         let mut buf: Vec<u8> = Vec::new();
-        solve(&mut f, &mut buf);
+        solve(&mut test_r, &mut buf);
 
         let res = String::from_utf8(buf).expect("valid string");
         //assert_eq!(res,
