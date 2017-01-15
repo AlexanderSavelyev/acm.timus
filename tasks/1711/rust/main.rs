1c718df5a428205aaaf2594bed6af4d33cd10f78
Aleksandr Savelev <aleksandr_savelev@epam.com>
2017-01-15 11:09:05 +0300

    start 1711

--- tasks/1711/rust/main.rs
+++ tasks/1711/rust/main.rs
@@ -8,11 +8,15 @@ fn solve(input: &mut Read, output: &mut Write) {
 
     reader.read_line(&mut input).unwrap();
     let n: i32 = input.trim().parse().unwrap();
-
+    let mut lines: Vec<String> = Vec::new();
+    let mut words: Vec<Vec<String>> = Vec::new();
     for _ in 0..n {
-        input.clear();
-        reader.read_line(&mut input).unwrap();
-        println!("{:?}", input);
+        let mut next_line = String::new();
+        reader.read_line(&mut next_line).unwrap();
+        lines.push(next_line);
+        // println!("{:?}", input);
+
+
         // let mut s = input.trim().split(' ');
 
         // let a_str = s.next().unwrap();
@@ -23,10 +27,20 @@ fn solve(input: &mut Read, output: &mut Write) {
 
         // println!("{} {}", a,b);
     }
-
-    // println!("{}", n);
-
-
+    input.clear();
+    reader.read_line(&mut input).unwrap();
+    let order = input.trim().split(' ');
+    for o in order {
+        let mut next: Vec<String> = Vec::new();
+        let b: usize = o.trim().parse().unwrap();
+        let next_input = &lines[b - 1];
+        let n_spl = next_input.trim().split(' ');
+        for ns in n_spl {
+            next.push(ns.to_string());
+        }
+        words.push(next);
+    }
+    println!("{:?}", words);
 }
 
 fn main() {
@@ -40,12 +54,19 @@ mod tests {
 
     #[test]
     fn basic_test() {
-        let test = String::from("5
-              ab
-              acb
-              bc
-              abac
-              babbc");
+        let test = String::from("11
+cipher grille kamkohob
+names codenames codes
+newtests rejudge timus
+size volume summit
+watchmen braineater twosides
+solution random yesorno
+keywords subversion commands
+bosses shooting shaitan
+game strategy playgame
+mnemonic palindromes bestname
+eligibility rectangle rules
+2 1 7 10 9 6 11 3 8 4 5");
         //let mut f = File::open("../input.txt").expect("correct test");
         let testb = test.into_bytes();
         let mut test_r = testb.as_slice();
