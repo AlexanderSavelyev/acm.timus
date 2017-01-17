759ad2017197f5e7ebfff10383f1a4bb3c779173
Aleksandr Savelev <aleksandr_savelev@epam.com>
2017-01-18 01:38:52 +0300

    starting solution

--- tasks/1711/rust/main.rs
+++ tasks/1711/rust/main.rs
@@ -1,6 +1,112 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 
+#[derive (Default, Debug)]
+struct Meta {
+    pub level: usize,
+    pub idx: usize,
+}
+#[derive (Debug)]
+struct Node {
+    pub meta: Meta,
+    pub nodes: Option<Vec<usize>>,
+}
+
+struct Tree {
+    pub node_pool: Vec<Node>,
+}
+
+impl Node {
+    pub fn new(m: Meta) -> Node {
+        Node {
+            meta: m,
+            nodes: None,
+        }
+    }
+}
+
+impl Tree {
+    pub fn new() -> Tree {
+        let mut tree = Tree { node_pool: Vec::new() };
+        tree.node_pool.push(Node::new(Meta::default()));
+        tree
+    }
+
+    fn get_root(&self) -> usize {
+        return 0;
+    }
+
+    fn get_node<'a>(&'a self, n_idx: usize) -> &'a Node {
+        self.node_pool.get(n_idx).unwrap()
+    }
+
+    fn insert_edge(&mut self, parent: usize, child: usize)  {
+        {
+            let mut p = self.node_pool.get_mut(parent).unwrap();
+            if p.nodes.is_none() {
+                p.nodes = Some(Vec::new())
+            }
+        }
+        let mut p = self.node_pool.get_mut(parent).unwrap().nodes.as_mut().unwrap();
+        p.push(child);
+    }
+
+    fn insert_node(&mut self, meta: Meta) -> usize {
+        let res = self.node_pool.len();
+        self.node_pool.push(Node::new(meta));
+        return res
+    }
+
+    fn has_path(&self, start: usize, end: usize) -> bool {
+        let mut has_path = false;
+        let mut node_stack: Vec<usize> = Vec::new();
+        node_stack.push(start);
+
+        while !node_stack.is_empty() && !has_path {
+            let cur_node = node_stack.pop().unwrap();
+            let c_node = self.get_node(cur_node);
+            if c_node.nodes.is_some() {
+                for n in c_node.nodes.as_ref().unwrap() {
+                    node_stack.push(*n);
+                    if *n == end {
+                        has_path = true;
+                    }
+                }
+            }
+        }
+
+        return has_path;
+    }
+    fn traverse_path(&self, cur_node: usize, end: usize, path: &mut Vec<usize>) -> bool {
+        let c_node = self.get_node(cur_node);
+        if c_node.nodes.is_some() {
+            for n in c_node.nodes.as_ref().unwrap() {
+                path.push(*n);
+                if *n == end {
+                    return true;
+                }
+                let res = self.traverse_path(*n, end, path);
+                if res  {
+                    return true;
+                }
+                path.pop();
+            }
+        } 
+        return false;
+    }
+
+    fn search_path(&self, start: usize, end: usize) -> Option<Vec<usize>> {
+        let mut res: Vec<usize> = Vec::new();
+        println!("Start search from {:?} to {:?}", start, end);
+        //res.push(start);
+        let has_path = self.traverse_path(start, end, &mut res);
+        if has_path {
+            return Some(res);
+        }
+        return None;
+    }
+
+}
 
 fn solve(input: &mut Read, output: &mut Write) {
     let mut reader = BufReader::new(input);
@@ -10,6 +116,7 @@ fn solve(input: &mut Read, output: &mut Write) {
     let n: i32 = input.trim().parse().unwrap();
     let mut lines: Vec<String> = Vec::new();
     let mut words: Vec<Vec<String>> = Vec::new();
+    let mut word_nodes: Vec<Vec<usize>> = Vec::new();
     for _ in 0..n {
         let mut next_line = String::new();
         reader.read_line(&mut next_line).unwrap();
@@ -27,8 +134,63 @@ fn solve(input: &mut Read, output: &mut Write) {
             next.push(ns.to_string());
         }
         words.push(next);
+        word_nodes.push(Vec::new());
     }
-    println!("{:?}", words);
+    //println!("{:?}", words);
+
+    //Start building tree
+    let mut tree = Tree::new();
+
+    let root = tree.get_root();
+
+    for level in 0 .. words.len() - 1 {
+        let n_level = level + 1;
+
+        let w = &words[level];
+        let n_w = &words[n_level];
+        word_nodes[level].resize(w.len(), 0);
+        word_nodes[n_level].resize(n_w.len(), 0);
+        let mut no_parent = true;
+        for idx in 0 .. w.len() {
+            for n_idx in 0 .. n_w.len() {
+                if w[idx] < n_w[n_idx] {
+                    no_parent = false;
+                    if word_nodes[level][idx] == 0 {
+                        word_nodes[level][idx] = tree.insert_node(Meta{level:level, idx: idx});
+                    }
+                    if word_nodes[n_level][n_idx] == 0 {
+                        word_nodes[n_level][n_idx] = tree.insert_node(Meta{level:n_level, idx: n_idx});
+                    }
+                    if level == 0 {
+                        tree.insert_edge(root, word_nodes[level][idx]);
+                    }
+                    tree.insert_edge(word_nodes[level][idx], word_nodes[n_level][n_idx]);
+                }
+            }
+        }
+
+        if no_parent {
+            writeln!(output, "IMPOSSIBLE").expect("correct output");
+            return;
+        }
+    }
+
+    // We have a path
+    let last_level = word_nodes.len() - 1;
+    for w in &word_nodes[last_level] {
+        if *w > 0 {
+            let path = tree.search_path(0, *w);
+            //println!("{:?}", path);
+            if path.is_some() {
+                for w in path.unwrap() {
+                    let n = tree.get_node(w);
+                    writeln!(output, "{}", words[n.meta.level][n.meta.idx]).expect("correct output");;
+                }
+                return;
+            }
+        }
+    }
+    writeln!(output, "IMPOSSIBLE").expect("correct output");
 }
 
 fn main() {
@@ -37,7 +199,6 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
     use solve;
 
     #[test]
@@ -62,11 +223,25 @@ eligibility rectangle rules
         solve(&mut test_r, &mut buf);
 
         let res = String::from_utf8(buf).expect("valid string");
-        //assert_eq!(res,
-//                   "2297.0716
-//936297014.1164
-//0.0000
-//37.7757
-//");
+        assert_eq!(res,
+                  "codenames\ngrille\nkeywords\nmnemonic\nplaygame\nrandom\nrectangle\nrejudge\nshooting\nsize\nwatchmen\n");
+    }
+
+    #[test]
+    fn basic_test2() {
+        let test = String::from("3
+problems in the
+first sample are
+ordered not randomly
+1 2 3");
+        //let mut f = File::open("../input.txt").expect("correct test");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut test_r, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+        assert_eq!(res,
+                  "IMPOSSIBLE\n");
     }
 }
