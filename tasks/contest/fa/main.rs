bfa0343d8561e0e53a16b5dfe580149d5f6405bd
Aleksandr Savelev <aleksandr_savelev@epam.com>
2017-02-24 20:04:42 +0300

    start fa

---
+++ tasks/contest/fa/main.rs
@@ -0,0 +1,65 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+
+fn solve(input: &mut Read, output: &mut Write) {
+    let mut reader = BufReader::new(input);
+    let mut input = String::new();
+
+    reader.read_line(&mut input).unwrap();
+    let a:u32;
+    let b:u32;
+    {
+        let mut s = input.trim().split(' ');
+
+        a = s.next().unwrap().parse().unwrap();
+        b = s.next().unwrap().parse().unwrap();
+    }
+    for _ in 0..(a+b) {
+        input.clear();
+        reader.read_line(&mut input).unwrap();
+        println!("{:?}", input);
+        // let mut s = input.trim().split(' ');
+
+        // let a_str = s.next().unwrap();
+        // let a: i32 = a_str.trim().parse().unwrap();
+
+        // let b_str = s.next().unwrap();
+        // let b: i32 = b_str.trim().parse().unwrap();
+
+        // println!("{} {}", a,b);
+    }
+
+    // println!("{}", n);
+
+
+}
+
+fn main() {
+    solve(&mut io::stdin(), &mut io::stdout());
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use solve;
+
+    #[test]
+    fn basic_test() {
+        let test = String::from("2 4
+1100 1100 2000 2400
+3000 3300
+1100 1160
+1190 1200 2000 2050
+3280 3300 3500 3550
+1500 1560");
+        //let mut f = File::open("../input.txt").expect("correct test");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut test_r, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+        assert_eq!(res, "a");
+    }
+}
