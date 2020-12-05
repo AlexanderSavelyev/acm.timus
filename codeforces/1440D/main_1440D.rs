f3d985ae1b7f7eebb1adccc2c33dc6a6866275fa
savelyev.alex <savelyev.alex@gmail.com>
2020-12-05 21:00:32 +0300

    dbitset for components (until 56 test, memory out)

--- codeforces/1440D/main_1440D.rs
+++ codeforces/1440D/main_1440D.rs
@@ -19,15 +19,13 @@ struct Graph {
 
 #[allow(dead_code)]
 impl Vertex {
-    fn new(self_id: usize) -> Vertex {
-        let mut nei: HashSet<usize> =  HashSet::new();
-        nei.insert(self_id);
+    fn new() -> Vertex {
         Vertex {
-            nei_vert: nei,
+            nei_vert: HashSet::new(),
         }
     }
     fn get_num_nei(&self)->usize {
-        return self.nei_vert.len() - 1;
+        return self.nei_vert.len();
     }
 }
 
@@ -41,7 +39,7 @@ impl Graph {
     }
     fn add_vertex(&mut self) -> usize {
         let res_idx = self.vertices_pool.len();
-        self.vertices_pool.push(Vertex::new(res_idx));
+        self.vertices_pool.push(Vertex::new());
         self.vertices_set.insert(res_idx);
         return res_idx;
     }
@@ -147,9 +145,6 @@ fn remove_vertices_min_nei(graph: &mut Graph, min_nei: usize) {
                 let next_vertex = graph.get_vertex(next_idx);
                 if next_vertex.get_num_nei() < min_nei {
                     for nei in &next_vertex.nei_vert {
-                        if *nei == next_idx {
-                            continue;
-                        }
                         vertices_queue.insert(*nei);
                     }
                     graph.remove_vertex(next_idx);
@@ -267,6 +262,8 @@ fn find_clique_max(graph: &Graph, k: usize) -> Option<HashSet<usize>> {
     return res;
 }
 
+
+#[allow(dead_code)]
 fn find_clique(graph: &mut Graph, k: usize) -> Option<HashSet<usize>> {
     let mut vertices_to_remove: Vec<usize> = Vec::new();
     loop {
@@ -608,6 +605,163 @@ fn find_clique_bitset(graph: &mut Vec<DBitset>, vert_map: &mut HashMap<usize, us
     return None;
 }
 
+struct BitsetGraph {
+    bitset_vec: Vec<DBitset>,
+    vert_map: Vec<usize>,
+    vert_len: Vec<usize>,
+    b_invmap: HashMap<usize, usize>,
+}
+
+
+fn build_bitset_graph(graph: &Graph, component: &HashSet<usize>) -> BitsetGraph {
+    let comp_len = component.len();
+    let mut bitset_vec: Vec<DBitset> = Vec::with_capacity(comp_len);
+    let mut vert_map: Vec<usize> = Vec::with_capacity(comp_len);
+    let mut vert_len : Vec<usize> = Vec::with_capacity(comp_len);
+
+    let mut b_map: HashMap<usize, usize> = HashMap::new();
+
+    for &v in component {
+        b_map.insert(v, vert_map.len());
+        bitset_vec.push(DBitset::new(comp_len));
+        vert_map.push(v);
+        vert_len.push(0);
+    }
+
+    for &v in component {
+        let bv = *b_map.get(&v).expect("correct component");
+        for nei in &graph.get_vertex(v).nei_vert {
+            bitset_vec[bv].set(*b_map.get(nei).expect("correct component"));
+        }
+        bitset_vec[bv].set(bv);
+        vert_len[bv] = graph.get_vertex(v).nei_vert.len();
+    }
+
+    return BitsetGraph {
+        bitset_vec: bitset_vec,
+        vert_map: vert_map,
+        vert_len: vert_len,
+        b_invmap: b_map,
+    }
+}
+
+fn find_result(graph: &mut Graph, 
+                        component: &HashSet<usize>, 
+                        clique: &mut Option<HashSet<usize>>, 
+                        result_set: &mut Option<HashSet<usize>>,
+                        k: usize) {
+    let mut bitset_graph: Option<BitsetGraph> = None;
+    if clique.is_none() {
+        bitset_graph = Some(build_bitset_graph(&graph, component));
+    }
+    let mut vertex_to_remove: Vec<usize> = Vec::new();
+    let mut vertices_to_update: Vec<usize> = Vec::new();
+
+    let mut comp_vert = component.clone();
+    loop {
+        if comp_vert.len() < k {
+            break;
+        }
+        vertex_to_remove.clear();
+        for &v in &comp_vert {
+            match &bitset_graph {
+                Some(b_graph) => {
+                    let bv = *b_graph.b_invmap.get(&v).expect("correct map");
+                    let vert_len = b_graph.vert_len[bv];
+                    // println!("vertex {} len {}", v+1, vert_len);
+                    if vert_len < k - 1 {
+                        vertex_to_remove.push(v);
+                        continue;
+                    } else if vert_len >= k {
+                        continue;
+                    }
+                    if clique.is_some() {
+                        vertex_to_remove.push(v);
+                        continue;
+                    }
+
+                    let clique_candidates = &b_graph.bitset_vec[bv];
+                    let mut is_clique = true;
+                    let mut bit = clique_candidates.next_set_bit(0);
+                    while bit.is_some() {
+                        let nei = bit.unwrap();
+                        if nei == bv {
+                            bit = clique_candidates.next_set_bit(nei + 1);
+                            continue;
+                        }
+
+                        if !clique_candidates.is_subset_of(&b_graph.bitset_vec[nei]) {
+                            is_clique = false;
+                            break;
+                        }
+                        bit = clique_candidates.next_set_bit(nei + 1);
+                    }
+
+                    if is_clique {
+                        // println!("is clique");
+                        let mut clique_c: HashSet<usize> = HashSet::new();
+                        bit = clique_candidates.next_set_bit(0);
+                        while bit.is_some() {
+                            let nei = bit.unwrap();
+                            clique_c.insert(b_graph.vert_map[nei]);
+                            bit = clique_candidates.next_set_bit(nei + 1);
+                        }
+                        clique.replace(clique_c);
+                    } 
+
+                },
+                None => {
+                    if graph.get_vertex(v).nei_vert.len() >= k {
+                        continue;
+                    }
+                }
+            }
+
+
+            vertex_to_remove.push(v);
+        }
+
+        // println!("vertex_to_remove {:?}", vertex_to_remove);
+        
+        if vertex_to_remove.len() == 0 {
+            if comp_vert.len() > k {
+                result_set.replace(comp_vert.clone());
+            }
+            break;
+        }
+
+        for &v in &vertex_to_remove {
+            comp_vert.remove(&v);
+        }
+
+        match bitset_graph.as_mut() {
+            Some(b_graph) => {
+                for v in &vertex_to_remove {
+                    vertices_to_update.clear();
+                    let bv = *b_graph.b_invmap.get(&v).expect("correct map");
+                    let clique_candidates = &b_graph.bitset_vec[bv];
+                    let mut bit = clique_candidates.next_set_bit(0);
+                    while bit.is_some() {
+                        let nei = bit.unwrap();
+                        vertices_to_update.push(nei);
+                        bit = clique_candidates.next_set_bit(nei + 1);
+                    }
+                    
+                    for nei in &vertices_to_update {
+                        b_graph.bitset_vec[*nei].reset(bv);
+                        b_graph.vert_len[*nei] = b_graph.vert_len[*nei] - 1;
+                    }
+                }
+            },
+            None => {
+                for &v in &vertex_to_remove {
+                    graph.remove_vertex(v);
+                }
+            }
+        }
+    }
+}
+
 fn solve(input: &mut dyn Read, output: &mut dyn Write) {
     let mut reader = BufReader::new(input);
     let mut input = String::new();
@@ -625,6 +779,8 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
         let n: usize = s.next().unwrap().trim().parse().unwrap();
         let m: usize = s.next().unwrap().trim().parse().unwrap();
         let k: usize = s.next().unwrap().trim().parse().unwrap();
+
+        // println!("test case {} {} {}", n, m, k);
 
         let mut graph: Graph = Graph::new();
 
@@ -654,47 +810,79 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
             continue;
         }
 
-        // let connected_components = graph.get_components();
+        let connected_components = graph.get_components();
 
-        // let mut clique: Option<&HashSet<usize>> = None;
+        let mut clique: Option<HashSet<usize>> = None;
+        let mut result_set: Option<HashSet<usize>> = None;
 
-        // for component in &connected_components {
-        //     // println!("component.len() {}", component.len());
-        //     if component.len() == k {
-        //         clique = Some(component);
-        //         break;
-        //     }
-        // }
-        let mut clique_graph = graph.clone();
+        for component in &connected_components {
+            // println!("component.len() {}", component.len());
+            if component.len() == k {
+                if clique.is_some() {
+                    continue;
+                }
+                clique = Some(component.clone());
+                continue;
+            }
+            
+            find_result(&mut graph, &component, &mut clique, &mut result_set, k);
+            if result_set.is_some() {
+                break;
+            }
+
+        }
+
+        match result_set {
+            Some(vertices) => {
+                writeln!(output, "1 {}", vertices.len()).expect("correct output");
+                let collected_vertices: Vec<String> =  vertices.iter().map(|&v| (v + 1).to_string()).collect();
+//              // collected_vertices.sort();
+                writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
+            },
+            None => {
+                match clique {
+                    Some(vertices) => {
+                        writeln!(output, "2").expect("correct output");
+                        let collected_vertices: Vec<String> =  vertices.iter().map(|&v| (v + 1).to_string()).collect();
+                        // collected_vertices.sort();
+                        writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
+                    },
+                    None => {
+                        writeln!(output, "-1").expect("correct output");
+                    }
+                }
+            }
+        }
+        // let mut clique_graph = graph.clone();
         // let mut clique_graph = make_dbitset_from(&graph);
         // let mut vert_map: HashMap<usize, usize> = HashMap::with_capacity(graph.vertices_pool.len());
         // for v in graph.get_vertices() {
         //     vert_map.insert(*v, graph.get_vertex(*v).nei_vert.len());
         // }
 
-        remove_vertices_min_nei(&mut graph, k);
+        // remove_vertices_min_nei(&mut graph, k);
 
-        if graph.get_num_vertices() > 0 {
-            writeln!(output, "1 {}", graph.get_num_vertices()).expect("correct output");
-            let collected_vertices: Vec<String> =  graph.get_vertices().iter().map(|&v| (v + 1).to_string()).collect();
-            // collected_vertices.sort();
-            writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
-        } else {
-            // let component_clique = find_clique_bitset(&mut clique_graph, &mut vert_map, k);
-            let component_clique = find_clique(&mut clique_graph, k);
+        // if graph.get_num_vertices() > 0 {
+        //     writeln!(output, "1 {}", graph.get_num_vertices()).expect("correct output");
+        //     let collected_vertices: Vec<String> =  graph.get_vertices().iter().map(|&v| (v + 1).to_string()).collect();
+        //     // collected_vertices.sort();
+        //     writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
+        // } else {
+        //     // let component_clique = find_clique_bitset(&mut clique_graph, &mut vert_map, k);
+        //     let component_clique = find_clique(&mut clique_graph, k);
 
-            match component_clique {
-                Some(vertices) => {
-                    writeln!(output, "2").expect("correct output");
-                    let collected_vertices: Vec<String> =  vertices.iter().map(|&v| (v + 1).to_string()).collect();
-                    // collected_vertices.sort();
-                    writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
-                },
-                None => {
-                    writeln!(output, "-1").expect("correct output");
-                }
-            }
-        }
+        //     match component_clique {
+        //         Some(vertices) => {
+        //             writeln!(output, "2").expect("correct output");
+        //             let collected_vertices: Vec<String> =  vertices.iter().map(|&v| (v + 1).to_string()).collect();
+        //             // collected_vertices.sort();
+        //             writeln!(output, "{}", collected_vertices.join(" ")).expect("correct output");
+        //         },
+        //         None => {
+        //             writeln!(output, "-1").expect("correct output");
+        //         }
+        //     }
+        // }
     }
 }
 
@@ -707,7 +895,7 @@ mod tests {
     use solve;
 
     #[test]
-    fn basic_test() {
+    fn basic_test1() {
         let test = String::from(
             "3
             5 9 4
@@ -760,4 +948,131 @@ mod tests {
 "
         );
     }
+    #[test]
+    fn basic_test3() {
+        let test = String::from(
+            "2
+            6 7 3
+            1 2
+            1 3
+            2 3
+            3 4
+            4 5
+            3 6
+            6 5
+            2 1 2
+            1 2"
+        );
+        //let mut f = File::open("../input.txt").expect("correct test");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut test_r, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+                assert_eq!(
+                    res,
+                    "2
+3 1 2
+2
+1 2
+"
+        );
+    }
+
+    #[test]
+    fn basic_test5() {
+        let test = String::from(
+            "7
+            7 5 2
+            3 5
+            1 3
+            2 6
+            1 4
+            1 2
+            3 1 1
+            2 3
+            5 7 5
+            1 5
+            1 2
+            2 3
+            2 4
+            1 3
+            3 5
+            3 4
+            6 11 4
+            1 3
+            1 2
+            1 5
+            5 6
+            3 5
+            4 5
+            1 6
+            2 6
+            1 4
+            4 6
+            3 4
+            12 22 6
+            2 4
+            3 5
+            10 11
+            1 6
+            4 5
+            3 10
+            7 9
+            4 12
+            2 8
+            3 12
+            9 11
+            2 9
+            11 12
+            1 11
+            3 9
+            2 12
+            1 2
+            2 10
+            10 12
+            5 8
+            4 10
+            7 12
+            8 12 5
+            5 6
+            6 8
+            1 6
+            3 8
+            6 7
+            3 4
+            3 5
+            2 5
+            1 2
+            1 3
+            2 7
+            4 6
+            2 1 2
+            1 2",
+        );
+        //let mut f = File::open("../input.txt").expect("correct test");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut test_r, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+                assert_eq!(
+                    res,
+                    "2
+4 1
+1 2
+2 3
+-1
+2
+3 1 5 4
+-1
+-1
+2
+1 2
+"
+        );
+    }
+    
 }
