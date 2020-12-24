4f15e017f261f7977be04a4c52e86012e209db94
savelyev.alex <savelyev.alex@gmail.com>
2020-12-25 00:55:55 +0300

    add segment tree

--- codeforces/1439C/main_1439C.rs
+++ codeforces/1439C/main_1439C.rs
@@ -1,7 +1,104 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 
+#[derive(Debug)]
+struct Node {
+    range_left: usize,
+    range_right: usize,
+    range_max: usize,
+    range_sum: usize,
+    nei1: usize,
+    nei2: usize,
+}
 
+struct SegmentTree {
+    nodes_pool: Vec<Node>,
+    root_node: usize,
+}
+
+impl Node {
+    fn new() -> Node {
+        Node {
+            range_left: 0,
+            range_right: 0,
+            range_max: 0,
+            range_sum: 0,
+            nei1: 0,
+            nei2: 0, 
+        }
+    }
+}
+
+impl SegmentTree {
+    fn new() -> SegmentTree {
+        return SegmentTree{
+            nodes_pool: Vec::new(),
+            root_node: 0,
+        };
+    }
+
+    fn build_from(&mut self, values: &Vec<usize>) {
+        let mut stack: Vec<usize> = Vec::new();
+        self.nodes_pool.push(Node::new());
+        for &v in values {
+            stack.push(self.nodes_pool.len());
+            self.nodes_pool.push(Node{
+                range_left: v,
+                range_right: v,
+                range_max: v,
+                range_sum: v,
+                nei1: 0,
+                nei2: 0, 
+            });
+        }
+        let mut stack_next: Vec<usize> = Vec::new();
+        let mut next_node: Node = Node::new();
+        // println!("{:?}", next_node);
+        // let mut counter: usize = 0;
+        loop {
+            for &nei in &stack {
+                let nei_node = &self.nodes_pool[nei];
+                // println!("nei_node {:?}", nei_node);
+                if next_node.nei1 == 0 {
+                    next_node.nei1 = nei;
+                    next_node.range_left = nei_node.range_left;
+                    if nei_node.range_max > next_node.range_max {
+                        next_node.range_max = nei_node.range_max;
+                    }
+                    next_node.range_sum = next_node.range_sum + nei_node.range_sum;
+                } else if next_node.nei2 == 0 {
+                    next_node.nei2 = nei;
+                    next_node.range_right = nei_node.range_right;
+                    if nei_node.range_max > next_node.range_max {
+                        next_node.range_max = nei_node.range_max;
+                    }
+                    next_node.range_sum = next_node.range_sum + nei_node.range_sum;
+                    println!("next_node {:?}", next_node);
+                    stack_next.push(self.nodes_pool.len());
+                    self.nodes_pool.push(next_node);
+                    next_node = Node::new();
+                }
+            }
+            if next_node.nei1 > 0 {
+                stack_next.push(self.nodes_pool.len());
+                self.nodes_pool.push(next_node);
+                next_node = Node::new();
+            }
+            if stack_next.len() == 1 {
+                self.root_node = stack_next[0];
+                break;
+            }
+            stack.resize(stack_next.len(), 0_usize);
+            stack.copy_from_slice(stack_next.as_slice());
+            stack_next.clear();
+            // counter += 1;
+            // if counter > 10 {
+            //     break;
+            // }
+        }
+        
+    }
+}
 fn solve(input: &mut dyn Read, output: &mut dyn Write) {
     let mut reader = BufReader::new(input);
     let mut input = String::new();
@@ -15,12 +112,18 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
     input.clear();
     reader.read_line(&mut input).unwrap();
 
-    let mut prices: Vec<usize> = Vec::new();
-    let mut p = input.trim().split(' ');
+    let mut prices: Vec<usize> = Vec::with_capacity(n);
+    let p = input.trim().split(' ');
 
     for a in p {
-        prices.push(a);
+        prices.push(a.parse().unwrap());
     }
+
+    println!("{:?}", prices);
+
+    let mut segment_tree = SegmentTree::new();
+
+    segment_tree.build_from(&prices);
 
     for _ in 0..q {
         input.clear();
@@ -31,9 +134,19 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
         let t: usize = s1.next().unwrap().trim().parse().unwrap();
         let x: usize = s1.next().unwrap().trim().parse().unwrap();
         let y: usize = s1.next().unwrap().trim().parse().unwrap();
+
+        println!("{} {} {}", t, x, y);
+
+        if t == 1 {
+
+        } else {
+            
+        }
+
     }
 
     // println!("{}", n);
+    writeln!(output, "1").expect("correct output");
 
 }
 
@@ -62,11 +175,8 @@ mod tests {
         solve(&mut test_r, &mut buf);
 
         let res = String::from_utf8(buf).expect("valid string");
-        //assert_eq!(res,
-//                   "2297.0716
-//936297014.1164
-//0.0000
-//37.7757
-//");
+        assert_eq!(res,
+                  "1
+");
     }
 }
