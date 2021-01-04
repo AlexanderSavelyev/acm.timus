c3b4706c3e8c39b5402f1477149549e25990d9ec
savelyev.alex <savelyev.alex@gmail.com>
2021-01-04 20:45:28 +0300

    C is solved

--- codeforces/1472C/main_1472C.rs
+++ codeforces/1472C/main_1472C.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufReader};
 use std::io::prelude::*;
+use std::cmp;
 
 const MAX_SIZE: usize = 200001;
 
@@ -11,7 +12,7 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
     let t: usize = input.trim().parse().unwrap();
 
     let mut a: Vec<usize> = vec![0; MAX_SIZE];
-    let mut visited: Vec<u8> = vec![0; MAX_SIZE];
+    let mut sums: Vec<usize> = vec![0; MAX_SIZE];
 
     for _ in 0..t {
         input.clear();
@@ -21,35 +22,37 @@ fn solve(input: &mut dyn Read, output: &mut dyn Write) {
 
         input.clear();
         reader.read_line(&mut input).unwrap();
-        
+
         let s = input.trim().split(' ');
-        let mut a_idx = 1;
+        let mut a_idx = 0_usize;
         for ai in s {
             let next_a: usize = ai.parse().unwrap();
-            a[a_idx] = next_a;
-            visited[a_idx] = 0;
             a_idx += 1;
+            a[a_idx] = next_a;
+            sums[a_idx] = 0;
         }
         let mut max_sum = 0_usize;
-        let mut next_idx;
-        let mut next_sum;
-        for i in 1..=n {
-            next_idx = i;
-            if visited[next_idx] == 1 {
-                continue;
-            }
-            next_sum = 0_usize;
 
-            while next_idx <= n {
-                next_sum += a[next_idx];
-                visited[next_idx] = 1;
-                next_idx += a[next_idx];
-            }
-            if next_sum > max_sum {
-                max_sum = next_sum;
+        // for i in 1..=n {
+            // println!("a {} {}", i, a[i]);
+        // }
+
+        for i in 1..=n {
+            let sum = a[i] + sums[i];
+            // println!("sum {}", sum);
+            let next_idx = i + a[i];
+            // println!("next_idx {}", next_idx);
+            if next_idx > n {
+                max_sum = cmp::max(max_sum, sum);
+                // println!("max_sum {}", max_sum);
+            } else {
+                sums[next_idx] =  cmp::max(sums[next_idx], sum);
+                // println!("put sum {} {}", next_idx, sum);
             }
         }
-
+        // for i in 1..=n {
+        //     println!("sums {} {}", i, sums[i]);
+        // }
         writeln!(output, "{}", max_sum).expect("valid string");
 
     }
@@ -66,9 +69,24 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use solve;
+    #[test]
+    fn basic_test1() {
+        let test = String::from("1
+3
+2 1 4");
+        let testb = test.into_bytes();
+        let mut test_r = testb.as_slice();
+        let mut buf: Vec<u8> = Vec::new();
+        solve(&mut test_r, &mut buf);
+
+        let res = String::from_utf8(buf).expect("valid string");
+        assert_eq!(res,
+                  "6
+");
+    }
 
     #[test]
-    fn basic_test() {
+    fn basic_test2() {
         let test = String::from("4
 5
 7 3 1 2 3
